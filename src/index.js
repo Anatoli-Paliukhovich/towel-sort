584d3e5b6bb23d38c816154f94996fabedfe27d6
“Anatoli-Paliukhovich” <humanities5050@gmail.com>
2023-08-10 14:50:46 +0300

    feat: update the task

--- src/index.js
+++ src/index.js
@@ -4,9 +4,7 @@
 module.exports = function towelSort(matrix = []) {
 	let result = [];
 	for (let i = 0; i < matrix.length; i++) {
-		if (matrix.length === 0) {
-			result = [];
-		} else if (i % 2) {
+		if (i % 2) {
 			result = result.concat(matrix[i].reverse());
 		} else {
 			result = result.concat(matrix[i]);
