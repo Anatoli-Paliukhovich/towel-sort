7035458a766e3530b2df602fd453d00237151e21
“Anatoli-Paliukhovich” <humanities5050@gmail.com>
2023-08-10 13:24:40 +0300

    feat: resolve the task

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
+module.exports = function towelSort(matrix) {
+	let result = [];
+	for (let i = 0; i < matrix.length; i++) {
+		if (i % 2) {
+			result = `${result},${matrix[i].reverse()},`;
+		} else {
+			result = `${result}${matrix[i]}`;
+		}
+	}
+	return [result.split(',')];
 }
